1b3ab80e32ec8adf4cb88bab297f050b5c460170
jdkfx <amateurranch@gmail.com>
2019-08-14 01:21:26 +0900

    TemplateMethod

--- TemplateMethod/templateMethod/Main.java
+++ TemplateMethod/templateMethod/Main.java
@@ -3,9 +3,9 @@ package templateMethod;
 public class Main {
 
 	public static void main(String[] args) {
-		AbstractDisplay d1 = new CharDisplay('H');
-		AbstractDisplay d2 = new StringDisplay("Hello, World!");
-		AbstractDisplay d3 = new StringDisplay("こんにちは！");
+		AbstractDisplay d1 = new CharDisplay('A');
+		AbstractDisplay d2 = new StringDisplay("Hello World!");
+		AbstractDisplay d3 = new StringDisplay("おはよう！");
 		d1.display();
 		d2.display();
 		d3.display();
