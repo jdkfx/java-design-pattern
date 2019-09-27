851df605fe061c462e2c1012fd82603b0054ccbf
jdkfx <amateurranch@gmail.com>
2019-09-27 13:47:41 +0900

    Command

---
+++ Command/drawer/DrawCommand.java
@@ -0,0 +1,17 @@
+package drawer;
+
+import java.awt.Point;
+
+import command.Command;
+
+public class DrawCommand implements Command {
+	protected Drawable drawable;
+	private Point position;
+	public DrawCommand(Drawable drawable, Point position) {
+		this.drawable = drawable;
+		this.position = position;
+	}
+	public void execute() {
+		drawable.draw(position.x, position.y);
+	}
+}
